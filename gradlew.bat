f379bdda98faee35c77a7a2e20326b678bbddb17
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-18 18:17:44 +0200

    Simplify gradlew.bat by removing comments
    Removed comments and updated the command to run the Gradle wrapper.

--- gradlew.bat
+++ gradlew.bat
@@ -1,9 +1,3 @@
 @echo off
-REM ------------------------------------------------------------------------------
-REM Gradle start up script for Windows
-REM ------------------------------------------------------------------------------
-
 SET DIRNAME=%~dp0
-SET GRADLE_HOME=%DIRNAME%\gradle\wrapper
-
-"%GRADLE_HOME%\gradlew" %*
+"%DIRNAME%\gradle\wrapper\gradle-wrapper.jar" %*
